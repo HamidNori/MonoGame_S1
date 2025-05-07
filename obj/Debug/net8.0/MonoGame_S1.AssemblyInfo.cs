fae40647e19e17dd0da3a1de96b3b492cb9a2b2a
HamidNori <hano07001@utb.vaxjo.se>
2025-05-07 10:53:20 +0200

    saker

--- obj/Debug/net8.0/MonoGame_S1.AssemblyInfo.cs
+++ obj/Debug/net8.0/MonoGame_S1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGame_S1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e28a707aa01e0d8bed4b573395724de499d4f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc92cc99a654883d63b62869b3c75bb3b5a15931")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGame_S1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGame_S1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
